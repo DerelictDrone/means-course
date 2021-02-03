31bce7f5710ac64a9f54b0c2436f1e049348f0ad
DerelictDrone <57756830+DerelictDrone@users.noreply.github.com>
2021-02-03 08:10:17 -0600

    updating code to show people

--- src/app/posts/post-list/post-list.component.ts
+++ src/app/posts/post-list/post-list.component.ts
@@ -1,4 +1,4 @@
-import { Component } from "@angular/core";
+import { Component, Input } from "@angular/core";
 
 @Component({
   selector: 'app-post-list',
@@ -12,5 +12,5 @@ export class PostListComponent {
     {title: 'Post 3', content: 'C'}
   ];
 }*/
-posts = [];
+  @Input() posts = [{title: '', content: ''},];
 };
