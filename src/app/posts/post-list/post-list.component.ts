48677dc78669aa072bcbcc6295acaba3961a786f
DerelictDrone <57756830+DerelictDrone@users.noreply.github.com>
2021-02-02 18:31:11 -0600

    all of the code

---
+++ src/app/posts/post-list/post-list.component.ts
@@ -0,0 +1,16 @@
+import { Component } from "@angular/core";
+
+@Component({
+  selector: 'app-post-list',
+  templateUrl: 'post-list.component.html',
+  styleUrls: ['post-list.component.css']
+})
+export class PostListComponent {
+/*  posts = [
+    {title: 'Why does god make me suffer', content: 'Aaaaaaaa'},
+    {title: 'Why does god make meeee', content: 'Bweee'},
+    {title: 'Why does god', content: 'Cweeee'}
+  ];
+}*/
+posts = [];
+};
